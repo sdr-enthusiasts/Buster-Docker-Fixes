6ee6ace62c2062ba7652ea9f6876a284136e84a9
kx1t <ramonkolb@gmail.com>
2022-02-07 17:43:05 -0500

    updates to support Debian Stretch, documentation updates

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Buster Docker Fixes
+# Buster (and Stretch) Docker Fixes
 
 ## The situation
 
-Users running Debian "Buster"-based operating systems (e.g., Raspbian/Raspberry Pi OS 1.3) on ARM hardware (e.g., Raspberry Pi) may have issues running certain Docker containers.
+Users running Debian "Stretch" or "Buster"-based operating systems (e.g., Raspbian/Raspberry Pi OS 1.3) on ARM hardware (e.g., Raspberry Pi) may have issues running certain Docker containers.
 
 Newer Docker containers can be based on Debian "Bullseye", the latest stable branch of Debian Linux. Newer versions of Linux are generally better maintained and receive timely bug fixes and security updates for installed packages.
 
 ## The problem
 
-Users of older "Buster" based ARM systems may experience problems running "Bullseye" based Docker Containers. For example, you may see errors in the logs indicating issues such as:
+Users of older "Stretch" or "Buster" based ARM systems may experience problems running "Bullseye" based Docker Containers. For example, you may see errors in the logs indicating issues such as:
 
 * With the `RTC`
 * `Real Time Clock`
@@ -21,17 +21,17 @@ This is an example of such error message:
 sleep: cannot read realtime clock: Operation not permitted
 ```
 
-The issue with "Buster" systems is related to a system package called `libseccomp2`. "Bullseye" Docker containers require a more up-to-date `libseccomp2` than is typically available on these older "Buster" systems.
+The issue with "Buster" systems is related to a system package called `libseccomp2`. "Bullseye" Docker containers require a more up-to-date `libseccomp2` than is typically available on these older systems.
 
 ## How to fix this
 
 ### ~~Four~~ Three options to fix
 You have ~~four~~ three options to ensure this container will work on your Pi.
 
-1. Update `libseccomp2` in your operating system 
+1. Update `libseccomp2` in your operating system
 2. Update to a fresh install of Raspberry Pi OS 1.4 (Debian "Bullseye"-based), or an install of Raspberry Pi OS from an image made after November 2021
 3. Upgrade to Ubuntu ARM 64 bit
-4. ~~Run this container with the `priviledged` flag.~~ **SECURITY ISSUE: NOT RECOMMENDED, PLEASE DO NOT DO THIS**
+4. ~~Run this container with the `privileged` flag.~~ **SECURITY ISSUE: NOT RECOMMENDED, PLEASE DO NOT DO THIS**
 
 ### Our recommendation
 The easiest solution is option 1: update `libseccomp2` in your operating system. [KX1T](https://github.com/kx1t) has created a [script](libseccomp2-checker.sh) that you can run. It will check that your system is "Buster" based and install an updated version of `libseccomp2` only if required and available.
@@ -48,14 +48,17 @@ After updating `libseccomp2`, you may have to restart your containers to ensure 
 
 ### What does the script do?
 
-The script will only work on "Buster"-based Debian distributions and will only update `libseccomp2` if it is outdated.
+The script will only work on "Stretch" or "Buster"-based Debian distributions and will only update `libseccomp2` if it is outdated.
 The `libseccomp2` script will do the following things to your system:
 
 * Determine if your system is buster based, and if not, stop
-* Update your system packages
+* Update your system packages and install `w3m` (needed to parse some HTML data for Stretch OS fixes)
 * If the script determines `libseccomp2` is outdated, it will then do the following after you give it permission to continue:
   - Add an official Debian repository to your apt sources along with the associated GPG key
   - Install a new version of `libseccomp2`
 
 You may be prompted for a password because the script is modifying things that require escalated (`sudo`) privileges.
 Feel free to inspect the script [here](libseccomp2-checker.sh).
+
+## LICENSE
+This repository, including any scripts, data, SDKs, and documentation is subject to the MIT License, [included](LICENSE) with this package. Copyright (c) 2021, 2022 by Ramon F. Kolb (kx1t), Fred Clausen, Mike Nye, and others.
--- libseccomp2-checker.sh
+++ libseccomp2-checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# This script updates a BUSTER Debian distribution so it will have the latest version of libseccomp2.
+# This script updates a STRETCH or BUSTER Debian distribution so it will have the latest version of libseccomp2.
 # The upgrade is necessary to run Bullseye-based Docker containers on a Buster host system.
 # For more information about this issue, please see https://docs.linuxserver.io/faq#option-2 and https://github.com/linuxserver/docker-jellyfin/issues/71#issuecomment-733621693
 #
@@ -13,10 +13,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,7 +26,7 @@
 # SOFTWARE.
 #
 # Welcome message:
-echo "Welcome to the libseccomp2 upgrade script for Buster. This script is meant to be run on Raspberry Pi Buster-based systems that have Docker containers"
+echo "Welcome to the libseccomp2 upgrade script for Debian Stretch and Buster. This script is meant to be run on Raspberry Pi Buster-based systems that have Docker containers"
 echo "that run Debian Bullseye or later. For this, the \"libseccomp2\" library version must be 2.4 or later."
 echo "This script will check this, and upgrade the library as necessary."
 echo ""
@@ -35,11 +35,12 @@ echo ""
 
 # Make sure we are indeed running a Debian Buster (Raspberry Pi OS) system:
 OS_VERSION="$(sed -n 's/\(^\s*VERSION_CODENAME=\)\(.*\)/\2/p' /etc/os-release)"
+[[ "$OS_VERSION" == "" ]] && OS_VERSION="$(sed -n 's/^\s*VERSION=.*(\(.*\)).*/\1/p' /etc/os-release)"
 OS_VERSION=${OS_VERSION^^}
-if [[ "$OS_VERSION" != "BUSTER" ]] && [[ "${1,,}" != "override" ]]
+if [[ "$OS_VERSION" != "BUSTER" ]] && [[ "$OS_VERSION" != "STRETCH" ]] && [[ "${1,,}" != "override" ]]
 then
-	echo "You aren't running BUSTER. The system reports $OS_VERSION."
-	echo "This script has been optimized for Raspberry Pi OS \"BUSTER\". Aborting."
+	echo "You aren't running Debian STRETCH or BUSTER. The system reports this OS: $OS_VERSION."
+	echo "This script has been optimized for Raspberry Pi OS \"STRETCH\" and \"BUSTER\". Aborting."
 	echo ""
 	echo "If you are 100% sure what you are doing and want to go ahead despite this warning,"
 	echo "you can skip this check by downloading the script and running it with the OVERRIDE commandline parameter:"
@@ -63,14 +64,14 @@ echo "Your system is \"${OS_VERSION}\" based, and it has libseccomp2 v${LIBVERSI
 echo "We will first update your system with the latest package versions. Please be patient, this may take a while."
 read -rp "Press ENTER to the upgrade or Control-C to cancel" </dev/tty
 echo ""
-sudo apt update -q && sudo apt upgrade -y -q
+sudo apt update -q && sudo apt upgrade -y -q && sudo apt install -y -qq w3m
 echo ""
 
 
 # Now check once more which version of libseccomp2 is installed, because the apt upgrade may have already installed a suitable version:
 LIBVERSION_MAJOR="$(apt-cache policy libseccomp2 | grep -e libseccomp2: -A1 | tail -n1 | sed -n 's/.*:\s*\([0-9]*\).\([0-9]*\).*/\1/p')"
 LIBVERSION_MINOR="$(apt-cache policy libseccomp2 | grep -e libseccomp2: -A1 | tail -n1 | sed -n 's/.*:\s*\([0-9]*\).\([0-9]*\).*/\2/p')"
-if (( LIBVERSION_MAJOR < 2 )) || (( LIBVERSION_MAJOR == 2 && LIBVERSION_MINOR < 4 ))
+if (( LIBVERSION_MAJOR < 2 )) || (( LIBVERSION_MAJOR == 2 && LIBVERSION_MINOR < 4 )) && [[ "${OS_VERSION}" == "BUSTER" ]]
 then
 	# We need to upgrade
 	echo "Now upgrading libseccomp2..."
@@ -78,6 +79,11 @@ then
 	echo "deb http://deb.debian.org/debian buster-backports main" | sudo tee -a /etc/apt/sources.list.d/buster-backports.list
 	sudo apt update
 	sudo apt install -y -q -t buster-backports libseccomp2
+elif (( LIBVERSION_MAJOR < 2 )) || (( LIBVERSION_MAJOR == 2 && LIBVERSION_MINOR < 4 )) && [[ "${OS_VERSION}" == "STRETCH" ]]
+then
+  INSTALL_CANDIDATE=$(curl -qsL http://ftp.debian.org/debian/pool/main/libs/libseccomp/ | w3m -T text/html -dump | sed -n 's/^.*\(libseccomp2_2.5.*armhf.deb\).*/\1/p' | sort | tail -1)
+  curl -qsL -o /tmp/"${INSTALL_CANDIDATE}" http://ftp.debian.org/debian/pool/main/libs/libseccomp/${INSTALL_CANDIDATE}
+  sudo dpkg -i /tmp/"${INSTALL_CANDIDATE}" && rm -f /tmp/"${INSTALL_CANDIDATE}"
 fi
 
 # Now make sure all went well
@@ -91,6 +97,6 @@ then
 	[[ ${A:0:1} == "y" ]] && docker restart $(docker ps -q)
 	echo "Done!"
 else
-	echo "Something went wrong. Please try running the script again! If that doesn't work, please file an issue at https://github.com/sdr-enthusiasts/Buster-Docker-Fixes/issues" 
+	echo "Something went wrong. Please try running the script again! If that doesn't work, please file an issue at https://github.com/sdr-enthusiasts/Buster-Docker-Fixes/issues"
 	echo "and we will try to help you."
 fi
